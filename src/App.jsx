cbf7f2a755462b9b4179ce4378f4a5d7788a87e5
Akansha Kumariz <akanshakumari25@navgurukul.org>
2026-09-10 22:32:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App(){
+  return (
+  <>
+  <h1>hello React</h1>
+  <ul>
+    <li>Javascript</li>
+    <li>java </li>
+    <li>C++</li>
+
+    
+    
+    </ul>
+  
+  </>
+  )
+
+}
+
+export default App
