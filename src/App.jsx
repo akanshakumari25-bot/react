d6556129f40f1781466cc10a03f45570da0763de
Akansha Kumariz <akanshakumari25@navgurukul.org>
2026-09-15 23:59:39 +0530

    Updated React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,27 @@
-import ProfileCard from "./Profilcard"
-import Header from  "./components/Header"
-import Sidebar from  "./components/Sidebar"
-import Main    from  "./components/Main"
-import Footer from "./components/Footer"
-import Profile from "./components/Profile"
+function Child({title,description}){
+  return(
+    <div>
+      <h2>{title}</h2>
+      <p>{description.toUpperCase()}</p>
+    </div>
 
-
-
-const user1 ={
-  name:"Alice",
-  age:28,
-  role:"Frontend developer"
-}
-const user2 ={
-  name:"Bob",
-  age:30,
-  role:"Backend Developer"
-}
-
-function App(){
-  return (
-  <>
-  <h1>hello React</h1>
-  <ul>
-    <li>Javascript</li>
-    <li>java </li>
-    <li>C++</li>
-    </ul>
- <ProfileCard/>
-  <Header/>
-  <div className="box">
-     <Sidebar/>
-    <Main/>
-  </div>
- 
- <Footer/>
-
-
-
- <Profile name={user1.name} age ={user1.age} role={user1.role}/>
- <Profile name={user2.name} age ={user2.age} role={user2.role}/>
-
- 
- 
- </>
   )
-
 }
 
+  function Parent(){
 
-export default App;
+    return(
+      <div>
+        <Child title="Hello World" description="This is a description " />
+      </div>
+    )
+  }
+
+
+
+export default Parent;
+
+
+// 
+
+
