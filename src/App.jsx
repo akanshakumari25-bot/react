e2ed6dfd9605c03c310b175cc500780992687d3e
Akansha Kumariz <akanshakumari25@navgurukul.org>
2026-09-15 09:10:31 +0530

    Add Profile component and props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import Header from  "./components/Header"
 import Sidebar from  "./components/Sidebar"
 import Main    from  "./components/Main"
 import Footer from "./components/Footer"
+import Profile from "./components/Profile"
 
+
+
+const user1 ={
+  name:"Alice",
+  age:28,
+  role:"Frontend developer"
+}
+const user2 ={
+  name:"Bob",
+  age:30,
+  role:"Backend Developer"
+}
 
 function App(){
   return (
@@ -23,11 +36,17 @@ function App(){
  
  <Footer/>
 
-  
-  </>
+
+
+ <Profile name={user1.name} age ={user1.age} role={user1.role}/>
+ <Profile name={user2.name} age ={user2.age} role={user2.role}/>
+
+ 
+ 
+ </>
   )
 
 }
 
-export default App
 
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+function Profile({name,age,role}){
+   return(
+        <div className="profilCard">
+            <h2>{name}</h2>
+            <p> Age:{age}</p>
+            <p> Role:{role}</p>
+        </div>
+    )
+
+}
+
+export default Profile;
+
