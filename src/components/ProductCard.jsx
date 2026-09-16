b85cfba73f2d85eb22680647c7998ab920b600cc
Akansha Kumariz <akanshakumari25@navgurukul.org>
2026-09-16 16:41:57 +0530

    updated code

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,28 +1,3 @@
-// function ProductCard({product,AddToCart }){
-//     return(
-//         <>
-//         <div>
-//             <h2>{product.name}</h2>
-//             <p>price:${product.price}</p>
-//             {product.inStock ?(<span>in stock</span>):<span>out of stock</span>} 
-//             <br />
-//             <button onClick={()=> AddToCart(product) }>Add to cart</button>
-//         </div>
-
-//         {/* <div>total items in cart</div> */}
-        
-//         </>
-
-
-
-//     )
-
-
-// }
-
-// export default ProductCard;
-
-
 
 
 function ProductCard({name,price,inStock}){
