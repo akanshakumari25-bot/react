c03abe6c833373836ea03fc515a14df357320339
Akansha Kumariz <akanshakumari25@navgurukul.org>
2026-09-16 09:34:22 +0530

    updated React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-function Child({title,description}){
-  return(
-    <div>
-      <h2>{title}</h2>
-      <p>{description.toUpperCase()}</p>
-    </div>
+import ProductCard from "./components/Product";
 
-  )
+const products =[
+{
+    id: 101,
+    name: "Wireless Mouse",
+    price: 29.99,
+},
+{
+  id: 102, 
+  name: "Mechanical Keyboard", 
+  price: 89.99 
+},
+{
+   id: 103,
+    name: "USB-C Hub", 
+    price: 45.00
+
+ },
+
+]
+
+
+function App() {
+  return (
+    <div>
+
+      <div className="products">
+        {products.map((product) => (
+          <div key={product.id}>
+            <ProductCard
+              name={product.name}
+              price={product.price}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-  function Parent(){
 
-    return(
-      <div>
-        <Child title="Hello World" description="This is a description " />
-      </div>
-    )
-  }
-
-
-
-export default Parent;
-
-
-// 
-
-
+export default App;
